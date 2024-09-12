f5b556be957a7f526ffcdd779eba43135c481afd
Vítězslav Dvořák <vitezslav.dvorak@spojenet.cz>
2024-09-12 13:10:35 +0200

    v0.1.0 release

--- src/pohoda-checker.php
+++ src/pohoda-checker.php
@@ -1,22 +1,40 @@
 <?php
+
+declare(strict_types=1);
+
 /**
- * PHPmServer - Pohoda Access probe
+ * This file is part of the PohodaClientChecker package
  *
- * @author     Vítězslav Dvořák <info@vitexsoftware.cz>
- * @copyright  (C) 2023-2024 Vitex Software
+ * https://github.com/Spoje-NET/pohoda-client-checker
+ *
+ * (c) Vítězslav Dvořák <http://vitexsoftware.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
+
 require_once '../vendor/autoload.php';
-\Ease\Shared::init(['POHODA_URL', 'POHODA_USERNAME', 'POHODA_PASSWORD'], array_key_exists(1, $argv) && file_exists($argv[1]) ? $argv[1] : '../.env');
+
+$options = getopt('o::e::', ['output::', 'environment::']);
+\Ease\Shared::init(['POHODA_URL', 'POHODA_USERNAME', 'POHODA_PASSWORD', 'POHODA_ICO'], \array_key_exists('environment', $options) ? $options['environment'] : '../.env');
+$destination = \array_key_exists('output', $options) ? $options['output'] : \Ease\Shared::cfg('RESULT_FILE', 'php://stdout');
+
 if (\Ease\Shared::cfg('EASE_LOGGER', false) === false) {
-    define('EASE_LOGGER', 'console');
+    \define('EASE_LOGGER', 'console');
 }
 
 $client = new \mServer\Client();
-if (\Ease\Shared::cfg('APP_DEBUG')) {
+
+if (strtolower(\Ease\Shared::cfg('APP_DEBUG', 'false')) === 'true') {
     $client->logBanner();
 }
 
 $result = $client->isOnline();
 $client->addStatusMessage(_('Connection') . ' ' . ($result ? 'OK' : 'problem'), $result ? 'success' : 'error');
-echo json_encode($result === false);
+$checkResult = ($result === false);
+
+$written = file_put_contents($destination, json_encode($checkResult, \Ease\Shared::cfg('DEBUG') ? \JSON_PRETTY_PRINT : 0));
+$client->addStatusMessage(sprintf(_('Saving result to %s'), $destination), $written ? 'success' : 'error');
+
+// ? exit($written ? 0 : 1);
 exit($result === false ? 1 : 0);
